b3a92590061497924587c6fefc237cc48fbc630b
Shimon Katsman <shkatsman@gmail.com>
2023-03-01 14:50:12 +0200

    added mail pages

--- apps/mail/pages/MailDetails.js
+++ apps/mail/pages/MailDetails.js
@@ -1,1 +1,22 @@
 // בס"ד
+
+export default {
+    props: [],
+    template: `
+    <h1>hi from details</h1>
+    `,
+    data() {
+        return {
+        }
+    },
+    methods: {
+       
+    },
+    computed: {
+    },
+    created() {
+    },
+    components: {
+    },
+    emits: [],
+}
--- apps/mail/pages/MailIndex.js
+++ apps/mail/pages/MailIndex.js
@@ -1,7 +1,31 @@
 // בס"ד
 
-export default{
-    template:`
-    <h1>hello</h1>
+import {storageService} from ''
+
+import MailList from '../cmps/MailList.js'
+
+export default {
+    props: [],
+    template: `
+    <section>
+    <h1>hi from index</h1>
+        <MailList v-if="mails" />
+    </section>
     `,
+    data() {
+        return {
+            mails: null,
+        }
+    },
+    methods: {
+    },
+    computed: {
+    },
+    created() {
+
+    },
+    components: {
+        MailList,
+    },
+    emits: [],
 }
