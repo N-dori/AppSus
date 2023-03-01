a4e63cd606615154ec444049e91307488ea6942c
Nadav Dori <dori.nadav@gmail.com>
2023-03-01 20:15:58 +0200

    added AddTxt component

--- apps/keep/pages/NoteIndex.js
+++ apps/keep/pages/NoteIndex.js
@@ -2,7 +2,7 @@
 import NotePreview from'../cmps/NotePreview.js'
 import { noteService } from '../services/note.service.js'
 import NoteHeader from'../cmps/NoteHeader.js'
-import NoteTxt from '../cmps/NoteTxt.js'
+import NoteList from '../cmps/NoteList.js'
 
 
 export default {
@@ -10,26 +10,29 @@ export default {
         <NoteHeader/>
 
     <section @click="" class="txt-editor">
-        <form @submit="">
-       <input @click="isShown=true, setTitle" class="txt-editor-title" type="text" 
+        <form @submit="pushNote">
+       <input @click="creatNewNote" @input="setTxt" class="txt-editor-title" type="text" 
              v-model="title"  placeholder="Take A note">
-      <div v-if="isShown" class="txt-editor-editor">
-       <input type="text" v-model="txt" placeholder="Take A note">
+      <div  class="txt-editor-editor">
+       <input type="text" v-model="body" @input="setTxt"  placeholder="Take A note">
        
        <i class="fa-regular fa-image"></i>
        <i class="fa-regular fa-palette"></i>
        <i class="fa-solid fa-list"></i>
        <i class="fa-brands fa-youtube"></i>
        </div>
+       <button type="submit">+</button>
     </form>
-       <pre>{{notes}}</pre>
+ 
    </section>
-   <NoteTxt  />
-    <NotePreview/>
+   <NoteList :notes="notes"/>
+ 
     `,
   data() {
         return {
-           title: "",
+            title:"",
+            body:"",
+            note:null,
             notes:[],
             filterBy: {},
             isShown:false,
@@ -42,9 +45,20 @@ export default {
 
     },
     methods:{
-        setTitle(){
-            console.log('set title', this. title);
+        setTxt(){
+          this.note.info.title=this.title
+          this.note.info.body=this.body
+        },
+        creatNewNote(){
+            const emptyNote=noteService.getEmptyNote()
+            this.note= emptyNote
+        
             
+        },pushNote(){
+       
+            noteService.save(this.note)
+            .then(note=>this.notes.push(note))
+
         }
 
     },computed:{
@@ -54,7 +68,7 @@ export default {
        noteService,
         NotePreview,
         NoteHeader,
-        NoteTxt,
+        NoteList,
   
     }
 
