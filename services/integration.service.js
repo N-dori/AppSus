183c9626df3ce44aa1a70fa590d984572f1c0533
Shimon Katsman <shkatsman@gmail.com>
2023-03-03 09:31:45 +0200

    work ready

--- services/integration.service.js
+++ services/integration.service.js
@@ -1,2 +1,35 @@
 // בס"ד
 
+export const integrationService = {
+    fromMailToNote,
+    fromNoteToMail,
+}
+
+function fromMailToNote(mail) {
+    return {
+        id: mail.id,
+        createdAt: Date.now(),
+        type: 'NoteTxt',
+        isPinned: false,
+        style: {
+            backgroundColor: '#fff'
+        },
+        info: {
+            title: mail.subject,
+            body: mail.body,
+        }
+    }
+}
+
+function fromNoteToMail(note) {
+    return {
+        id: note.id,
+        subject: note.title,
+        body: note.body,
+        isRead: false,
+        sentAt: null,
+        removedAt: null,
+        from: 'user@appsus.com',
+        to: '',
+    }
+}
